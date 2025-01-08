42fd03bbccd32b18d71b1f5f989487d515fa2ea7
Luquet4 <lucasgabrielvmb@gmail.com>
2025-01-07 21:00:15 -0400

    Sistema concluido

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def listar(lista):
     for i, pessoa in enumerate(lista):
         print(f"ID - {i} - {pessoa}")
 
-
 def selecionar_id(lista):
     while True:
         try:
@@ -40,6 +39,18 @@ def continuar(msg):
         else:
             print("Opção Inválida!")
 
+def gerar_codigo(lista, prefixo):
+    while True:
+        codigo = f"{prefixo}{randint(1000, 9999)}"
+        if codigo not in [item.codigo for item in lista]:
+            return codigo
+
+def gerar_nome(lista, nomes_possiveis):
+    while True:
+        nome = faker.random_element(nomes_possiveis)
+        if nome not in [item.nome for item in lista]:
+            return nome
+
 class Pessoas:
     def __init__(self, nome, matricula, data_nasc, sexo, endereco, telefone, email):
         validar_sexo = lambda x: x.upper() if x.upper() in ["M", "F"] else 'N/A'
@@ -58,7 +69,7 @@ class Pessoas:
                 lista_alunos.append(Pessoas(
                         nome=pedir_input("Nome do Aluno: ", type=str),
                         matricula=f"MAT{randint(1000, 9999)}",
-                        data_nasc=pedir_input("Data de Nascimento: ", type=int),
+                        data_nasc=pedir_input("Data de Nascimento: ", type=str),
                         sexo=pedir_input("M ou F - ", type=str),
                         endereco=pedir_input("Endereço: ", type=str),
                         telefone=pedir_input("Telefone: ", type=float),
@@ -91,7 +102,7 @@ class Professor(Pessoas):
                 lista_professores.append(Professor(
                         nome=pedir_input("Nome do professor: ", type=str),
                         matricula=f"MAT{randint(1000, 9999)}",
-                        data_nasc=pedir_input("Data de Nascimento: ", type=int),
+                        data_nasc=pedir_input("Data de Nascimento: ", type=str),
                         sexo=pedir_input("M ou F - ", type=str),
                         endereco=pedir_input("Endereço: ", type=str),
                         telefone=pedir_input("Telefone: ", type=float),
@@ -104,23 +115,6 @@ class Professor(Pessoas):
             except Exception as e:
                 print(f"Erro: {e}")
 
-    def atribuir_disciplina():
-        while True:
-            try:
-                print("Selecione o professor que deseja atribuir uma disciplina abaixo - \n")
-                prof = selecionar_id(lista_professores)
-
-                print(f"Escolha a disciplina para atribuir ao professor - {lista_professores[prof].nome}")
-                disc = selecionar_id(lista_disciplinas)
-
-                lista_professores[prof].disciplina = lista_disciplinas[disc].nome
-                lista_disciplinas[disc].professor.append(lista_professores[prof].nome)
-
-                if not continuar("Deseja atribuir outra disciplina? 1- Sim, 2- Não\n"):
-                    break
-            except Exception as e:
-                print(e)
-
     def __str__(self):
         return f"Nome: {self.nome}, Matrícula: {self.matricula}, Disciplina: {self.disciplina}, Data Nascimento: {self.data_nasc}, Sexo: {self.sexo}, Email: {self.email}, Telefone: {self.telefone}, Endereço: {self.endereco}"
 
@@ -130,7 +124,27 @@ class Disciplinas:
         self.codigo = codigo
         self.ch = carga_horaria
         self.professor = professor
-    
+
+    def cadastrar_disciplina():
+        while True:
+            try:
+                print("- CADASTRO DE DISCIPLINAS -\n")
+                nome = pedir_input("Nome da Disciplina: ", type=str)
+                if nome in [disciplina.nome for disciplina in lista_disciplinas]:
+                    print("Essa disciplina já está cadastrada!")
+                    continue
+                lista_disciplinas.append(Disciplinas(
+                    nome=nome, 
+                    codigo=f"A{randint(1000,1999)}", 
+                    carga_horaria=pedir_input("Carga Horária: ", type=int), 
+                    professor=[]
+                ))
+                listar(lista_disciplinas)
+                if not continuar("Cadastro realizado com sucesso! Deseja realizar novamente? 1- Sim, 2- Não\n"):
+                    break
+            except Exception as e:
+                print(e)
+
     def atribuir_professor():
         while True:
             try:
@@ -154,43 +168,37 @@ class Disciplinas:
             except Exception as e:
                  print(e)
 
-    def cadastrar_disciplina():
-        while True:
-            try:
-                print("- CADASTRO DE DISCIPLINAS -\n")
-                print("Seleciona o professor da Disciplina -\n")
-                indice_professor = selecionar_id(lista_professores)
-                
-                if not lista_professores[indice_professor].disciplina:
-                    lista_disciplinas.append(Disciplinas(
-                        nome=pedir_input("Nome da Disciplina: ", type=str), 
-                        codigo=f"A{randint(1000,1999)}", 
-                        carga_horaria=pedir_input("Carga Horária: ", type=int), 
-                        professor=[lista_professores[indice_professor].nome]
-                    ))
-                    lista_professores[indice_professor].disciplina = lista_disciplinas[-1].nome
-                    listar(lista_disciplinas)
-
-                    if not continuar("Cadastro realizado com sucesso! Deseja realizar novamente? 1- Sim, 2- Não\n"):
-                        break
-                else:
-                    print("Professor já associado a outra disciplina.")
-                    input("Pressione Enter para continuar...")
-                    continue
-            except Exception as e:
-                print(e)
-
-
     def __str__(self):
         return f"Nome: {self.nome}, Código: {self.codigo}, Carga Horária: {self.ch}, Professores Responsável: {self.professor}"
-    
-class Turmas():
+
+class Turmas:
     def __init__(self, nome, codigo, disciplina, professor, alunos):
         self.nome = nome
         self.codigo = codigo
         self.disciplina = disciplina
         self.professor = professor
         self.alunos = alunos
+
+    def cadastrar_turma():
+        while True:
+            try:
+                print("- CADASTRO DE TURMAS -\n")
+                nome = pedir_input("Nome da Turma: ", type=str)
+                if nome in [turma.nome for turma in lista_turmas]:
+                    print("Essa turma já está cadastrada!")
+                    continue
+                lista_turmas.append(Turmas(
+                    nome=nome, 
+                    codigo=f"A{randint(1000,1999)}", 
+                    disciplina=None, 
+                    professor=None, 
+                    alunos=[]
+                ))
+                listar(lista_turmas)
+                if not continuar("Cadastro realizado com sucesso! Deseja realizar novamente? 1- Sim, 2- Não\n"):
+                    break
+            except Exception as e:
+                print(e)
 
     def atribuir_disciplina_a_turma():
         while True:
@@ -208,16 +216,18 @@ class Turmas():
             except Exception as e:
                 print(e)
 
-    def listar_disciplinas_por_turma(turma_nome):
-        for turma in lista_turmas:
-            if turma.nome == turma_nome:
-                print(f"Disciplina da turma {turma.nome}: {turma.disciplina}")
-
     def __str__(self):
         return f"Nome: {self.nome}, Código: {self.codigo}, Disciplina: {self.disciplina}, Professor: {self.professor}, Alunos: {self.alunos}"
 
+
+
+lista_alunos = []
+lista_professores = []
+lista_disciplinas = []
+lista_turmas = []
+
 lista_alunos = [Pessoas(nome=faker.name(), 
-        matricula=f"MAT{randint(1000, 9999)}", 
+        matricula=gerar_codigo(lista_alunos, "MAT"), 
         data_nasc=faker.date_of_birth(), 
         sexo=faker.random_element(["M", "F"]), 
         endereco=faker.address(), 
@@ -227,7 +237,7 @@ lista_alunos = [Pessoas(nome=faker.name(),
 
 lista_professores = [Professor(
         nome=faker.name(), 
-        matricula=f"MAT{randint(1000, 9999)}", 
+        matricula=gerar_codigo(lista_professores, "MAT"), 
         data_nasc=faker.date_of_birth(), 
         sexo=faker.random_element(["M", "F"]), 
         endereco=faker.address(), 
@@ -236,35 +246,35 @@ lista_professores = [Professor(
         disciplina=None
     ) for _ in range(3)]
 
-lista_disciplinas = [Disciplinas(
-        nome=faker.random_element(["Matemática", "Inglês", "Português", "Algoritmos"]),
-        codigo=f"A{randint(1000, 1999)}",
-        carga_horaria=randrange(20, 120, 10),
-        professor=[]
-    ) for _ in range(3)]
+for _ in range(3):
+    nome = gerar_nome(lista_disciplinas, ["Matemática", "Inglês", "Português", "Algoritmos"])
+    codigo = gerar_codigo(lista_disciplinas, "A")
+    carga_horaria = randrange(20, 120, 10)
+    lista_disciplinas.append(Disciplinas(nome=nome, codigo=codigo, carga_horaria=carga_horaria, professor=[]))
 
-lista_turmas = [Turmas(
-        nome=faker.random_element(["TADS 1", "TADS 2", "TADS 3", "Engenharia da Computação 1"]),
-        codigo=f"A{randint(1000, 1999)}",
-        disciplina=lista_disciplinas[i],
-        professor=lista_professores[i].nome,
-        alunos=[lista_alunos[j].matricula for j in range(10)]
-        ) for i in range(3)]
-
+for i in range(3):
+    nome = gerar_nome(lista_turmas, ["TADS 1", "TADS 2", "TADS 3", "Engenharia da Computação 1"])
+    codigo = gerar_codigo(lista_turmas, "A")
+    disciplina = lista_disciplinas[i]
+    professor = lista_professores[i].nome
+    alunos = [lista_alunos[j].matricula for j in range(10)]
+    lista_turmas.append(Turmas(nome=nome, codigo=codigo, disciplina=disciplina, professor=professor, alunos=alunos))
 
 
 def menu():
     while True:
+        os.system("cls" if os.name == "nt" else "clear")
         print("\n- MENU PRINCIPAL -")
         print("1 - Matricular Aluno")
         print("2 - Cadastrar Professor")
         print("3 - Cadastrar Disciplina")
-        print("4 - Atribuir Professor à Disciplina")
-        print("5 - Atribuir Disciplina à Turma")
-        print("6 - Listar Alunos")
-        print("7 - Listar Professores")
-        print("8 - Listar Disciplinas")
-        print("9 - Listar Turmas")
+        print("4 - Cadastrar Turma")
+        print("5 - Atribuir Professor a Disciplina")
+        print("6 - Atribuir Disciplina a Turma")
+        print("7 - Listar Alunos")
+        print("8 - Listar Professores")
+        print("9 - Listar Disciplinas")
+        print("10 - Listar Turmas")
         print("0 - Sair")
         
         opcao = pedir_input("Escolha uma opção: ", type=int)
@@ -276,22 +286,27 @@ def menu():
         elif opcao == 3:
             Disciplinas.cadastrar_disciplina()
         elif opcao == 4:
-            Professor.atribuir_disciplina()
+            Turmas.cadastrar_turma()
         elif opcao == 5:
-            Turmas.atribuir_disciplina_a_turma()
+            Disciplinas.atribuir_professor()
         elif opcao == 6:
-            listar(lista_alunos)
+            Turmas.atribuir_disciplina_a_turma()
         elif opcao == 7:
-            listar(lista_professores)
+            listar(lista_alunos)
+            input("\nPressione ENTER para continuar...")
         elif opcao == 8:
-            listar(lista_disciplinas)
+            listar(lista_professores)
+            input("\nPressione ENTER para continuar...")
         elif opcao == 9:
+            listar(lista_disciplinas)
+            input("\nPressione ENTER para continuar...")
+        elif opcao == 10:
             listar(lista_turmas)
+            input("\nPressione ENTER para continuar...")
         elif opcao == 0:
             print("Saindo...")
             break
         else:
             print("Opção inválida, por favor, escolha uma opção válida!")
 
-# Chama o menu
 menu()
